d05dfec88f38921958c1254950beffa16135fe5e
JonathanChavezTamales <jonathanchatab@hotmail.com>
2025-08-01 11:46:06 -0700

    stuff

--- src/zeroeval/__init__.py
+++ src/zeroeval/__init__.py
@@ -7,8 +7,9 @@ from .core.task import task
 from .core.evaluation import evaluation
 from .core.metrics import column_metric, run_metric
 from .core.init import init
+from .providers import ZeroEvalOTLPProvider, SingleProcessorProvider, langfuse_zeroeval
 
-__all__ = ["Dataset", "task", "evaluation", "column_metric", "run_metric", "init"]
+__all__ = ["Dataset", "task", "evaluation", "column_metric", "run_metric", "init", "ZeroEvalOTLPProvider", "SingleProcessorProvider", "langfuse_zeroeval"]
 
 # Optional: Add version info
 __version__ = "0.1.0"
