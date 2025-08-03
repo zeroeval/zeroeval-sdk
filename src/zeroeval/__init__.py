ecf4353b5ae5e071f8a23d68c6048601bdd87d8f
Sebastian Crossa <sebastiancrossa@hotmail.com>
2025-08-03 12:09:26 -0700

    fix session context

--- src/zeroeval/__init__.py
+++ src/zeroeval/__init__.py
@@ -7,7 +7,7 @@ from .core.task import task
 from .core.evaluation import evaluation
 from .core.metrics import column_metric, run_metric
 from .core.init import init
-from .providers import ZeroEvalOTLPProvider, SingleProcessorProvider, langfuse_zeroeval
+from .providers import ZeroEvalOTLPProvider, SingleProcessorProvider
 
 __all__ = ["Dataset", "task", "evaluation", "column_metric", "run_metric", "init", "ZeroEvalOTLPProvider", "SingleProcessorProvider", "langfuse_zeroeval"]
 
